6cf4880b3798ba21e7890c04a50c83034c982dee
Chris <enecatalin9393@gmail.com>
2024-07-25 00:45:22 +0300

    Fix gateway, wasted day for nothing. Need to move config to config server

--- api-gateway/src/main/java/com/socialmedia/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/socialmedia/gateway/ApiGatewayApplication.java
@@ -2,12 +2,29 @@ package com.socialmedia.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+import reactor.netty.http.client.HttpClient;
 
+//@EnableConfigurationProperties(UriConfiguration.class)
+@RestController
 @SpringBootApplication
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes().build();
+
+	}
+
 
 }
